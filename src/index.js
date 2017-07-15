55b69c7b59e81827d22343bd611ae925c365941b
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-14 17:19:20 -0700

    search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/searchBar';
+
+const API_KEY = 'AIzaSyA8Qd9PyAiw5i0mYxCFbaB45ro2uYcb_64';
+
 const App = () => {
-  return <div>Hi!</div>
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
+
+export default App;
